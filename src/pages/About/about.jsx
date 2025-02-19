850333d02e24fdc0e503ebbc7894d88d711b3d0c
Bhavya <Ankitamalik03@gmail.com>
2025-02-19 17:33:52 +0530

    cjhanges

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import aboutheader from '../../assets/about/aboutheader.png';
+import aboutheader from '../../assets/about.png';
 import TableReservation from '../../components/Reservation/reservationComponent';
 import OurLocation from '../../components/ourlocation/ourlocation';
 import AboutGallery from '../../components/AboutGallery/aboutgallery';
@@ -8,11 +8,12 @@ import NavbarTwo from '../../components/Navbar/navbar2';
 import LogoSlider from '../../components/LogoSlider/logoslider';
 import Navbar from '../../components/Navbar/navbar';
 
+
 const About = () => {
   return (
     <div className="relative">
       <NavbarTwo />  
-      <div className="w-full bg-brown-200 pt-28">
+      <div className="w-full bg-white ">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="relative">
             <div className="aspect-[16/9] w-full overflow-hidden rounded-2xl">
@@ -27,11 +28,11 @@ const About = () => {
               </div>
             </div>
             
-            <div className="absolute bottom-8 left-0 right-0 flex justify-center">
+            {/* <div className="absolute bottom-8 left-0 right-0 flex justify-center">
               <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-[#e07e90] tracking-wider">
                 ABOUT US
               </h1>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
